910d74d9550ba4ba28bacd7503c64ac4a1d705a0
KimoK8122 <kimo.k@me.com>
2020-09-29 09:50:47 -0600

    update Part 3

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Testing.Models;
+
+namespace Testing
+{
+    public interface IProductRepository
+    { 
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+        public void UpdateProduct(Product product);
+    }
+}
